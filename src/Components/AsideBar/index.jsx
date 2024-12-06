1670e9299e2367248e1be1c31be426ebb2146a85
Edwin Miguel <edwinmiguelorellana@gmail.com>
2024-12-05 19:55:40 -0500

    fix: mueve la carpeta de assets a public

--- src/Components/AsideBar/index.jsx
+++ src/Components/AsideBar/index.jsx
@@ -5,7 +5,7 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/solid"
 const AsideBar = ({ children }) => {
   return (
     <aside className="flex flex-col w-[calc(100%/1)] h-full p-4 bg-black absolute left-0 col-start-1 col-end-1">
-      <img className="w-32" src="src/assets/logo_drive_blanco.png" alt="" />
+      <img className="w-32" src="/assets/logo_drive_blanco.png" alt="" />
       {children}
     </aside>
   )
